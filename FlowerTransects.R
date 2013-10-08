ead030160299d6b86c82aba700b4c8fec12711a3
bw4sz <benweinstein2010@gmail.com>
2013-10-08 18:16:11 -0400

    Began Merge with Holgar on going data

--- FlowerTransects.R
+++ FlowerTransects.R
@@ -1,21 +1,43 @@
 #Flower Diversity Transects and Nectar
-#Data Collected by K. Lohman and B. Weinstein
+#Data Collected by Holger Beck,K. Lohman and B. Weinstein
 #R script Ben Weinstein - Stony Brook University 7/7/2013
 #Under Git repository - Maquipucuna
 
+#Read in required libraries
 require(ggplot2)
 require(reshape2)
 require(maptools)
 library(plyr)
-#Setwd
-setwd("E:/FieldWork2013/Data2013")
 
-#Read in Flower Transect Data
-fl<-read.csv(file="csv/FlowerTransects.csv")
+#Set DropBox Working Directory
+
+setwd("C:/Users/Ben/Dropbox/")
+
+#Read in Flower Transect Data from summer field season
+fl<-read.csv(file="Thesis/Maquipucuna_SantaLucia/Data2013/csv/FlowerTransects.csv")
+
+#Load in holger's on going data
+holger.fl<-read.csv("Thesis/Maquipucuna_SantaLucia/Data2013/csv/HolgerTransects.csv")
+
+#Fix the column names and rbindfill
+colnames(fl)<-c("Family","Genus","Species","Height","Flowers","Stalks","Inflorescences.Plants","GPS_ID","Accuracy","Hummingbird.Species","Photo","Transect.ID","Comment")
+
+colnames(holger.fl)<-c("Transect.ID","Date","Time","Family","Genus","Species","Height","Flowers","Stalks","Inflorescences.Plants","Hummingbird.Species","GPS_ID","Comment","Photo")
+
+#To compare transects we need the transect id page
+TID<-read.csv("Thesis/Maquipucuna_SantaLucia/Data2013/csv/TransectIID.csv")
+TID$Transect_R<-as.factor(paste(TID$Elevation.Begin,TID$Elevation.End,sep="_"))
+
+#Select the flower transects
+TID.f<-TID[TID$Type=="Flower",]
+fl.id<-merge(fl,TID.f,by.x="Transect.ID",by.y="TransectID")
 
 #view flower data
-head(fl)
+head(fl.id)
 
+full.fl<-rbind.fill(fl.id,holger.fl[,1:14])
+
+head(full.fl)
 #Data cleaning and check
 #Turn first letter of family to uppercase
 #see toupper?
@@ -25,93 +47,95 @@ head(fl)
         sep="", collapse=" ")
 }
 
-fl$Family<-as.factor(sapply(fl$Family,function(x) .simpleCap(as.character(x))))
+full.fl$Family<-as.factor(sapply(full.fl$Family,function(x) .simpleCap(as.character(x))))
 #How many families do we have?
-fl.t<-melt(table(fl$Family))
+fl.t<-melt(table(full.fl$Family))
 names(fl.t)<-c("Family","Count")
 ggplot(fl.t,aes(Family,Count)) + geom_bar() + coord_flip() + theme_bw()
 
 
 #Turn genus to uppercase
-fl$Genus<-as.factor(sapply(fl$Genus,function(x) .simpleCap(as.character(x))))
-table(fl$Genus)
-fl.g<-melt(table(fl$Genus))
+full.fl$Genus<-as.factor(sapply(full.fl$Genus,function(x) .simpleCap(as.character(x))))
+table(full.fl$Genus)
+fl.g<-melt(table(full.fl$Genus))
 names(fl.g)<-c("Genus","Count")
 ggplot(fl.g,aes(Genus,Count)) + geom_bar() + coord_flip() + theme_bw()
 
 #######
 #MS species need to be subbed in for the best taxonomy known
-fl<-fl[!fl$Family %in% c("Ms1","Ms2"),]
+full.fl<-full.fl[!full.fl$Family %in% c("Ms1","Ms2"),]
 #for now just remove?
 
 ########################################################
 #Combination of family genus count
-fl.fg<-melt(table(paste(fl$Family,fl$Genus)))
+fl.fg<-melt(table(paste(full.fl$Family,full.fl$Genus)))
 names(fl.fg)<-c("F.Genus","Count")
 ggplot(fl.fg,aes(F.Genus,Count)) + geom_bar() + coord_flip() + theme_bw()
 
 #For species, turn all to lowercase
-fl$Species<-as.factor(sapply(fl$Species,tolower))
-fl.s<-melt(table(fl$Species))
+full.fl$Species<-as.factor(sapply(full.fl$Species,tolower))
+fl.s<-melt(table(full.fl$Species))
 names(fl.s)<-c("Species","Count")
 ggplot(fl.s,aes(Species,Count)) + geom_bar() + coord_flip() + theme_bw()
 
 #Family Genus Species, in future, create abreviations
-fl.all<-melt(table(paste(fl$Family,fl$Genus,fl$Species)))
+fl.all<-melt(table(paste(full.fl$Family,full.fl$Genus,full.fl$Species)))
 names(fl.all)<-c("Species","Count")
 ggplot(fl.all,aes(Species,Count)) + geom_bar() + coord_flip() + theme_bw()
+ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/SpeciesCount.jpeg",height=8,width=11)
+
 #Use this as the marker for now
-fl$Full<-paste(fl$Family,fl$Genus,fl$Species)
+full.fl$Full<-paste(full.fl$Family,full.fl$Genus,full.fl$Species)
 
 #Create total flower column by multiplying the Flowers per stalk, by stalks and total inflorescens
 #First need to take the average flowers per stalk, and all other columns
-fl$mean_flowerStalk<-sapply(fl$Flowers.Stalk,function(x) {
+full.fl$mean_flowerStalk<-sapply(full.fl$Flowers,function(x) {
+  mean(as.numeric(strsplit(as.character(as.list(x)[[1]]),",")[[1]]),na.rm=TRUE)
+})
+
+full.fl$mean_Stalk<-sapply(full.fl$Stalks,function(x) {
   mean(as.numeric(strsplit(as.character(as.list(x)[[1]]),",")[[1]]))
 })
 
-fl$mean_Stalk<-sapply(fl$Number_Stalks,function(x) {
+full.fl$mean_inflorescences<-sapply(full.fl$Inflorescences.Plants,function(x) {
   mean(as.numeric(strsplit(as.character(as.list(x)[[1]]),",")[[1]]))
 })
 
-fl$mean_inflorescences<-sapply(fl$Total_Plant.Inflorescenes,function(x) {
-  mean(as.numeric(strsplit(as.character(as.list(x)[[1]]),",")[[1]]))
-})
-
-fl$Total_Flowers<-fl$mean_flowerStalk*fl$mean_Stalk*fl$mean_inflorescences
+full.fl$Total_Flowers<-full.fl$mean_flowerStalk*full.fl$mean_Stalk*full.fl$mean_inflorescences
 hist(fl$Total_Flowers)
 
 #Visualize total flowers by species
-ggplot(data=fl,aes(Full,Total_Flowers)) + geom_boxplot() + coord_flip() #+ ylim(0,200)
+ggplot(data=full.fl,aes(Full,Total_Flowers)) + geom_boxplot() + coord_flip() #+ ylim(0,200)
 
 #visualize height by species
-ggplot(data=fl,aes(Full,as.numeric(Height.m.))) + geom_boxplot() + coord_flip()
+ggplot(data=full.fl,aes(Full,as.numeric(Height))) + geom_boxplot() + coord_flip()
 
 
 #Data cleaning on hummingbirds
+full.fl<-data.frame(full.fl,hum.split<-colsplit(as.character(full.fl$Hummingbird.Species),",",c("Hummingbird","Sex")))
 
-fl<-data.frame(fl,hum.split<-colsplit(as.character(fl$Hummingbird_Obs.),",",c("Hummingbird","Sex")))
-table(fl$Hummingbird)
-fl$Hummingbird<-as.factor(fl$Hummingbird)
-levels(fl$Hummingbird)[which(levels(fl$Hummingbird)=="White-whskered hermit")]<-"White-whiskered hermit"
-levels(fl$Hummingbird)[1]<-NA
+#Count of hummingbird records
+table(full.fl$Hummingbird)
+
+full.fl$Hummingbird<-as.factor(full.fl$Hummingbird)
+
+#Ignore blank cells
+levels(full.fl$Hummingbird)[1]<-NA
+
 #Hummingbirds on which flowers
-G.hum<-melt(table(fl$Genus,fl$Hummingbird))
+G.hum<-melt(table(full.fl$Genus,full.fl$Hummingbird))
+
 #remove the empty observations
 G.hum[G.hum$value==0,"value"]<-NA
 colnames(G.hum)<-c("Genus","Hummingbird","Observation")
 GH<-ggplot(G.hum,aes(x=Genus,y=Hummingbird,fill=Observation)) + geom_tile() + theme_bw() + scale_fill_continuous(high="red",na.value="white")
 GH + theme(axis.text.x=element_text(angle=90))
 
+ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/Hummingbird_Genus.jpeg",height=8,width=11)
+
 #########################
-
+#Skip this section for now 10/8/2013, needs to be redone to work with holgar data
 #Compare transects
-#To compare transects we need the transect id page
-TID<-read.csv("csv/TransectIID.csv")
-TID$Transect_R<-as.factor(paste(TID$Elevation.Begin,TID$Elevation.End,sep="_"))
-
-#Select the flower transects
-TID.f<-TID[TID$Type=="Flower",]
-fl.id<-merge(fl,TID.f,by.x="Transect.ID",by.y="TransectID")
 
 #aggregate flowers by range
 fl.agg<-aggregate(fl.id$Total_Flowers,by=list(fl.id$Transect_R,fl.id$Replicate.Number,fl.id$Date),sum,na.rm=TRUE)
