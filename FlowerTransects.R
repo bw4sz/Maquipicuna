0338ab685b87e1b3adaf8e81ac1d4d44d1bb4eef
bw4sz <benweinstein2010@gmail.com>
2013-10-08 20:57:11 -0400

    Developed Function to compute network metrics on slices on the data.

--- FlowerTransects.R
+++ FlowerTransects.R
@@ -8,9 +8,10 @@ require(ggplot2)
 require(reshape2)
 require(maptools)
 library(plyr)
+require(plotKML)
+require(reshape)
 
 #Set DropBox Working Directory
-
 setwd("C:/Users/Ben/Dropbox/")
 
 #Read in Flower Transect Data from summer field season
@@ -21,7 +22,6 @@ holger.fl<-read.csv("Thesis/Maquipucuna_SantaLucia/Data2013/csv/HolgerTransects.
 
 #Fix the column names and rbindfill
 colnames(fl)<-c("Family","Genus","Species","Height","Flowers","Stalks","Inflorescences.Plants","GPS_ID","Accuracy","Hummingbird.Species","Photo","Transect.ID","Comment")
-
 colnames(holger.fl)<-c("Transect.ID","Date","Time","Family","Genus","Species","Height","Flowers","Stalks","Inflorescences.Plants","Hummingbird.Species","GPS_ID","Comment","Photo")
 
 #To compare transects we need the transect id page
@@ -53,7 +53,6 @@ fl.t<-melt(table(full.fl$Family))
 names(fl.t)<-c("Family","Count")
 ggplot(fl.t,aes(Family,Count)) + geom_bar() + coord_flip() + theme_bw()
 
-
 #Turn genus to uppercase
 full.fl$Genus<-as.factor(sapply(full.fl$Genus,function(x) .simpleCap(as.character(x))))
 table(full.fl$Genus)
@@ -61,7 +60,6 @@ fl.g<-melt(table(full.fl$Genus))
 names(fl.g)<-c("Genus","Count")
 ggplot(fl.g,aes(Genus,Count)) + geom_bar() + coord_flip() + theme_bw()
 
-#######
 #MS species need to be subbed in for the best taxonomy known
 full.fl<-full.fl[!full.fl$Family %in% c("Ms1","Ms2"),]
 #for now just remove?
@@ -105,13 +103,15 @@ full.fl$Total_Flowers<-full.fl$mean_flowerStalk*full.fl$mean_Stalk*full.fl$mean_
 hist(fl$Total_Flowers)
 
 #Visualize total flowers by species
-ggplot(data=full.fl,aes(Full,Total_Flowers)) + geom_boxplot() + coord_flip() #+ ylim(0,200)
+ggplot(data=full.fl,aes(Full,Total_Flowers)) + geom_boxplot() + coord_flip()
 
 #visualize height by species
 ggplot(data=full.fl,aes(Full,as.numeric(Height))) + geom_boxplot() + coord_flip()
 
-
+################################################
 #Data cleaning on hummingbirds
+################################################
+
 full.fl<-data.frame(full.fl,hum.split<-colsplit(as.character(full.fl$Hummingbird.Species),",",c("Hummingbird","Sex")))
 
 #Count of hummingbird records
@@ -124,10 +124,10 @@ levels(full.fl$Hummingbird)[1]<-NA
 
 #Hummingbirds on which flowers
 G.hum<-melt(table(full.fl$Genus,full.fl$Hummingbird))
+colnames(G.hum)<-c("Genus","Hummingbird","Observation")
 
 #remove the empty observations
-G.hum[G.hum$value==0,"value"]<-NA
-colnames(G.hum)<-c("Genus","Hummingbird","Observation")
+G.hum[G.hum$Observation==0,"Observation"]<-NA
 GH<-ggplot(G.hum,aes(x=Genus,y=Hummingbird,fill=Observation)) + geom_tile() + theme_bw() + scale_fill_continuous(high="red",na.value="white")
 GH + theme(axis.text.x=element_text(angle=90))
 
@@ -138,77 +138,104 @@ ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/Hummingbird_Genus.jpeg",h
 #Compare transects
 
 #aggregate flowers by range
-fl.agg<-aggregate(fl.id$Total_Flowers,by=list(fl.id$Transect_R,fl.id$Replicate.Number,fl.id$Date),sum,na.rm=TRUE)
-names(fl.agg)<-c("Range","Replicate.Number","Date","Flowers")
+#fl.agg<-aggregate(fl.id$Total_Flowers,by=list(fl.id$Transect_R,fl.id$Replicate.Number,fl.id$Date),sum,na.rm=TRUE)
+#names(fl.agg)<-c("Range","Replicate.Number","Date","Flowers")
 
-ggplot(fl.agg,aes(y=Flowers,x=Range)) + geom_point()
+#ggplot(fl.agg,aes(y=Flowers,x=Range)) + geom_point()
 
 #How does the distribution of genera change with elevation
-g.r<-table(fl.id$Transect_R,fl.id$Genus)
-Record_Total<-apply(table(fl.id$Transect_R,fl.id$Genus),1,sum)
-prop.Genus<-g.r/Record_Total
-prop.Genus<-melt(prop.Genus)
+#g.r<-table(fl.id$Transect_R,fl.id$Genus)
+#Record_Total<-apply(table(fl.id$Transect_R,fl.id$Genus),1,sum)
+#prop.Genus<-g.r/Record_Total
+#prop.Genus<-melt(prop.Genus)
 
-prop.Genus<-prop.Genus[!prop.Genus$value %in% 0,]
-names(prop.Genus)<-c("Range","Genus","Proportion")
+#prop.Genus<-prop.Genus[!prop.Genus$value %in% 0,]
+#names(prop.Genus)<-c("Range","Genus","Proportion")
 
 #merge in family for facet
-prop.Genus$Family<-sapply(prop.Genus$Genus,function(x) {
-  unique(fl[fl$Genus %in% x,"Family"])})
+#prop.Genus$Family<-sapply(prop.Genus$Genus,function(x) {
+ # unique(fl[fl$Genus %in% x,"Family"])})
 
-prop.Genus<-prop.Genus[!prop.Genus$Range %in% "NA_NA",]
+#prop.Genus<-prop.Genus[!prop.Genus$Range %in% "NA_NA",]
 #How does the proportion of records change by elevation
-ggplot(prop.Genus,aes(x=Range,y=Proportion,fill=Genus)) + geom_bar(position="stack") 
-
+#ggplot(prop.Genus,aes(x=Range,y=Proportion,fill=Genus)) + geom_bar(position="stack") 
 
 ## Plot proportions by family, Genera as lines within each facet, just more than 3%
-p<-ggplot(prop.Genus[prop.Genus$Proportion > .05,],aes(x=Range,y=Proportion*100)) + geom_line(aes(group=Genus,col=Genus),size=1.2) + facet_wrap(~Family) + geom_point()
-p+ theme_bw()+theme(axis.text.x = element_text(angle = 90))
+#p<-ggplot(prop.Genus[prop.Genus$Proportion > .05,],aes(x=Range,y=Proportion*100)) + geom_line(aes(group=Genus,col=Genus),size=1.2) + facet_wrap(~Family) + geom_point()
+#p+ theme_bw()+theme(axis.text.x = element_text(angle = 90))
 
-m.TID.genus<-melt(table(fl.id$Transect_R,fl.id$Genus))
-colnames(m.TID.genus)<-c("Range","Genus","Count")
+#m.TID.genus<-melt(table(fl.id$Transect_R,fl.id$Genus))
+#colnames(m.TID.genus)<-c("Range","Genus","Count")
 
-ggplot(m.TID.genus,aes(x=Range,y=Genus,size=Count)) + geom_point() + coord_flip()
-m.fl<-melt(fl,id.var="Transect.ID")
+#ggplot(m.TID.genus,aes(x=Range,y=Genus,size=Count)) + geom_point() + coord_flip()
+#m.fl<-melt(fl,id.var="Transect.ID")
 
 #########################
+#Read and convert gpx points to a single dataframe and save it as a shapefile
+f<-list.files("Holger/Transect_Protocol_Holger/WayPoints/",full.names=TRUE)
+
+#loop through input files and find the errors. 
+gpx<-list()
+for (x in 1:length(f)){
+  print(x)
+  try(
+    gpx[[x]]<-readGPX(f[x],waypoints=TRUE)$waypoints)
+}
+
+##Repeat for first gps
+f<-list.files("F:\\KarenGPS\\KarenFirstgps/",full.names=TRUE)
+
+#loop through input files and find the errors. 
+gpx2<-list()
+for (x in 1:length(f)){
+  print(x)
+  try(
+    gpx2[[x]]<-readGPX(f[x],waypoints=TRUE)$waypoints)
+}
+#Bind together the days taht contain data
+gpx.dat<-rbind(rbind.fill(gpx[sapply(gpx,class)=="data.frame"]),rbind.fill(gpx2[sapply(gpx2,class)=="data.frame"]))
+
+#create  spatial object
+dat.sp<-SpatialPointsDataFrame(coords=cbind(gpx.dat$lon,gpx.dat$lat),gpx.dat)
+
 #Bring elevation gps data
-Elev<-lapply(list.files("F:\\FieldWork2013\\KarenGPS\\shp",pattern=".shp",full.names=T),readShapePoints)
+Elev<-lapply(list.files("Thesis/Maquipucuna_SantaLucia/Data2013/Shapefiles/AnushaGPS.shp",pattern=".shp",full.names=T),readShapePoints)
 Elev<-rbind.fill(lapply(Elev,as.data.frame))
 
 #Turn GPS ID into factor
-fl$GPS_ID<-as.numeric(fl$GPS_ID)
+full.fl$GPS_ID<-as.numeric(full.fl$GPS_ID)
 
 #add to 00 to anything less than 10
-fl$GPS_ID[fl$GPS_ID < 10]<-paste("00",fl$GPS_ID[fl$GPS_ID < 10],sep="")
-fl$GPS_ID[as.numeric(fl$GPS_ID) < 100 & as.numeric(fl$GPS_ID) >= 10 ]<-paste("0",fl$GPS_ID[as.numeric(fl$GPS_ID) < 100 & as.numeric(fl$GPS_ID) >= 10],sep="")
+full.fl$GPS_ID[full.fl$GPS_ID < 10]<-paste("00",full.fl$GPS_ID[full.fl$GPS_ID < 10],sep="")
+full.fl$GPS_ID[as.numeric(full.fl$GPS_ID) < 100 & as.numeric(full.fl$GPS_ID) >= 10 ]<-paste("0",full.fl$GPS_ID[as.numeric(full.fl$GPS_ID) < 100 & as.numeric(full.fl$GPS_ID) >= 10],sep="")
 
 #Match each point with an elevation
-fl.elev<-merge(fl,Elev,by.x="GPS_ID",by.y="name")
+fl.elev<-merge(full.fl,Elev,by.x="GPS_ID",by.y="name")
 
 #How records were in fl, but not matched
-fl[!fl$GPS_ID %in% fl.elev$GPS_ID,]$GPS_ID
+full.fl[!full.fl$GPS_ID %in% fl.elev$GPS_ID,]$GPS_ID
 
 #How many flowers at each elevation
 ggplot(fl.elev,aes(elevation,y=Total_Flowers)) + geom_line() + geom_point(aes(color=Family),size=2) + theme_bw() 
 p<-ggplot(fl.elev,aes(x=elevation,y=Total_Flowers,col=Species),size=2) + geom_point() + theme_bw() + facet_wrap(~Family,scales='free_y') + guides(color="none")
 p+stat_smooth(method='lm',aes(group=Family))
+
 #spatially?
 ggplot(fl.elev,aes(x=coords.x1,y=coords.x2,col=Total_Flowers),size=200) + theme_bw() + geom_point() + scale_color_continuous(high="red",low="blue",limits=c(0,200))
 
 #Bring in nectar data
-Nectar <- read.csv("F:/FieldWork2013/Data2013/csv/Nectar.csv")
+Nectar <- read.csv("Thesis/Maquipucuna_SantaLucia/Data2013/csv/Nectar.csv")
+
 #Fix colnames that are ugly
 colnames(Nectar)[c(8,10,11,12,13)]<-c("Height","TubeLength","Brix","EffectiveCorolla","TotalCorolla")
-  
 
 #Data Cleaning
 #All family and Genus should be capitalized
 Nectar$Family<-as.factor(sapply(Nectar$Family,function(x) .simpleCap(as.character(x))))
 Nectar$Genus<-as.factor(sapply(Nectar$Genus,function(x) .simpleCap(as.character(x))))
+
 #species should be lowercase
 Nectar$Species<-as.factor(sapply(Nectar$Species,function(x) tolower(as.character(x))))
-
 
 #Create full name column
 Nectar$Full<-paste(Nectar$Family,Nectar$Genus,Nectar$Species)
@@ -216,7 +243,6 @@ Nectar$Full<-paste(Nectar$Family,Nectar$Genus,Nectar$Species)
 #Some basic visualizations to check data clarity
 #number of records per species
 m.Nectar<-melt(table(Nectar[!is.na(Nectar$Brix),]$Full))
-
 
 #No records should be 0? turn these to NA
 Nectar[Nectar$Brix==0 & is.finite(Nectar$Brix),]
@@ -252,12 +278,10 @@ dim(missing)
 table(missing$Full)
 
 #okay, before we go crazy with nectar,lets look at corolla length with elevation
-
 ggplot(fl.nectar,aes(x=elevation,y=TotalCorolla,col=Family)) + geom_point() + stat_smooth(aes(group=1),method='lm')
 
 #Nectar concentration and elevation, there are some odd records that need to be fixed
 ggplot(fl.nectar,aes(x=elevation,y=Brix,col=Family)) + geom_point() + stat_smooth(aes(group=1),method='lm') + ylim(5,30)
 
 #create volume calculations
-
-save.image("/FieldWork2013/Data2013/Scripts/FlowerTransect.Rdata")
+save.image("Thesis/Maquipucuna_SantaLucia/Results/FlowerTransect.Rdata")
--- Network.R
+++ Network.R
@@ -1,42 +1,59 @@
+###############################################################################
+#Hummingbird Flower Interactions at the Santa Lucia and Maquipucuna EcoReserves
+###############################################################################
+#Ben Weinstein - Stony Brook University - Department of Ecology and Evolution
+
+require(chron)
 require(bipartite)
 require(ggplot2)
 require(ape)
 require(reshape)
 require(sna)
-#Read in flower videos
-setwd("C:/Users/Jorge/Dropbox/Thesis/Maquipucuna_SantaLucia/Data2013/csv/")
 
-dat<-read.csv("FlowerVideo.csv")
+#############################
+#Set Dropbox Location
+#Read in flower videos
+home<-"C:/Users/Ben/Dropbox/"
+setwd(home)
+#############################
+
+#############
+#Read in Data
+#############
+
+#Read in Flower Dataset
+dat<-read.csv("Thesis/Maquipucuna_SantaLucia/Data2013/csv/FlowerVideo.csv")
 
 #Bring in the phylogeny
-
 #Read in phylogeny
-tree<-read.nexus("C:/Users/Jorge/Dropbox/Shared Ben and Catherine/DimDivEntire/Files for Analysis/ColombiaPhylogenyUM.tre")
+tree<-read.nexus("Shared Ben and Catherine/DimDivEntire/Files for Analysis/ColombiaPhylogenyUM.tre")
 
 #Read in names file to replace names in Nexis file
-spnames<-read.table(file="C:/Users/Jorge/Dropbox/Shared Ben and Catherine/DimDivEntire/Files for Analysis/SpNameTree.txt" , sep = "\t", header = TRUE)
+spnames<-read.table(file="Shared Ben and Catherine/DimDivEntire/Files for Analysis/SpNameTree.txt" , sep = "\t", header = TRUE)
 
 #Replace tip.label with Spnames#
 tree$tip.label<-as.character(spnames$SpName) 
 head(dat)
 
 #For the sake of simplicity, make everything lowercase
-
 .simpleCap <- function(x) {
   s <- strsplit(x, " ")[[1]]
   paste(toupper(substring(s, 1,1)), substring(s, 2),
         sep="", collapse=" ")
 }
 
-
 #bring in clade data
-clades<-read.csv("C:\\Users/Jorge/Dropbox/Shared Ben and Catherine/DimDivEntire/Files for Analysis/CladeList.txt",header=FALSE)[,-1]
+clades<-read.csv("Shared Ben and Catherine/DimDivEntire/Files for Analysis/CladeList.txt",header=FALSE)[,-1]
 colnames(clades)<-c("Clade","Genus","Species","double","English")
 clades<-clades[,1:5]
 
 ####################################################
-#Begin Analysis
+#Analysis of Flower Usage for each Hummingbird Species
 ####################################################
+
+##############
+#Data Cleaning 
+##############
 
 #Capitalize Flowers
 dat$Flower<-factor(sapply(dat$Flower,function(x) {.simpleCap(as.character(x))}))
@@ -46,50 +63,86 @@ dat$Hummingbird<-factor(sapply(dat$Hummingbird,function(x) {.simpleCap(as.charac
 
 #Overall statistics
 #How many flowers Species
-nlevels(dat$Flower)
+paste("Number of Flower Species:",nlevels(dat$Flower))
 
 #How many Birds Species
-nlevels(dat$Hummingbird)
+paste("Number of Hummingbird Species:",nlevels(dat$Hummingbird))
 
+#Create function to compute network parameters
+#The general strategy is to write all metrics to file, and develop call statements at the end to retrieve them
+NetworkC<-function(datf,naming){
+  
+  #Set a working directory, create a folder for each run
+  setwd(home)
+  toset<-paste("Thesis/Maquipucuna_SantaLucia/Results/Network/",naming,sep="")
+  dir.create(toset,showWarnings=FALSE)
+  setwd(toset)
+
+  
 #Interaction of flowers and birds
-F_H<-as.data.frame.array(table(dat$Flower,dat$Hummingbird))
+F_H<-as.data.frame.array(table(datf$Flower,datf$Hummingbird))
 
-require(bipartite)
-?bipartite
+#Save Input Matrix
+write.csv(F_H,"BirdXFlower.csv")
 
 #View Web
+svg(filename="WebPlot.svg",height=7,width=12)
 plotweb(F_H)
+dev.off()
 
-#In a different sense
+#Plot matrix of interactions
+#There was an error on the svg
+jpeg(filename="MatrixPlot.jpeg",height=8,width=8,units="in",res=300)
 visweb(F_H)
+dev.off()
 
 #Metrics across entire
-birds.prop<-networklevel(F_H,level="higher")
-plants.prop<-networklevel(F_H,level="lower")
+birds.prop<-data.frame(HummingbirdNetwork=networklevel(F_H,level="higher"))
+plants.prop<-data.frame(PlantNetwork=networklevel(F_H,level="lower"))
 
-#Metrics across species
+#Merge networks
+NetworkProp<-data.frame(birds.prop,plants.prop)
+#Write to file
+write.csv(NetworkProp,"NetworkProperties.csv")
+  
+#Metrics across species, write to file
 H.species.prop<-specieslevel(F_H,level="higher")
-P.species.prop<-specieslevel(F_H,level="lower")
 
-#Specialization, but note the insane comment box ?nodespec, ask ppl who know better. 
-nodespec(F_H)
+#Hummingbird Properties
+write.csv(H.species.prop,"HummingbirdMetrics.csv")
+
+#Plant Network Metrics  
+P.species.prop<-specieslevel(F_H,level="lower")
+write.csv(P.species.prop,"PlantMetrics.csv")
+  
+##################################################
+#Specialization for each species
+##################################################
+#nodespec(F_H)
+
 #For plants
-dfun(F_H)
+#dfun(F_H)
+
 #For birds
 birds.special<-dfun(t(F_H))
 birds.spl<-data.frame(lapply(birds.special,data.frame))
 colnames(birds.spl)<-names(birds.special)
 ggplot(birds.spl,aes(x=rownames(birds.spl),y=dprime)) + geom_point() + theme_bw() + theme(axis.text.x=element_text(angle=90))
+ggsave("Specialization.svg",height=8,width=9)
 
-#How old is the taxa, color the above specialization graph.
-graph.bipartite(F_H)
-#As one way matrices 
+#############################################
+#Resource overlap between Hummingbird species
+#############################################
+
+#Collapse Matrix into Hummingbird by Hummingbird Matrix
 #Hummingbird
 H_H<-as.one.mode(F_H,project="higher")
 
 diag(H_H)<-NA
+H_H[upper.tri(H_H)]<-NA
 m.HH<-melt(H_H)
-ggplot(m.HH,aes(X1,X2,fill=value)) + geom_tile() + scale_fill_continuous(low="blue",high="red") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggplot(m.HH,aes(X1,X2,fill=value)) + geom_tile() + scale_fill_continuous(low="blue",high="red",na.value="white") + theme(axis.text.x = element_text(angle = 90, hjust = 1),panel.background=element_rect(color="white"))
+ggsave("ResourceOverlap.svg",height=8,width=11)
 
 #Relatedness and flower overlap, very rudimentary test so far
 ctrx<-cophenetic(tree)
@@ -109,15 +162,45 @@ ctrx<-cophenetic(tree)
 m.HH$Relatedness<-sapply(1:nrow(m.HH),ER)
 
 #Relatedness and plant overlap
-ggplot(m.HH[m.HH$value>0,],aes(y=value,x=as.numeric(Relatedness),)) + geom_point() + geom_smooth(method="lm") + theme_bw()
-
+ggplot(m.HH[m.HH$value>0,],aes(y=value,x=as.numeric(Relatedness),)) + geom_point() + geom_smooth(method="lm") + theme_bw() + ylab("Resource Overlap") + xlab("Relatedness")
+ggsave("Network/Relatedness_Overlap.svg",height=8,width=11)
 
 #Plants 
 P_P<-as.one.mode(F_H,project="lower")
+diag(P_P)<-NA
+P_P[upper.tri(P_P)]<-NA
 m.PP<-melt(P_P)
+
 #
-ggplot(m.PP,aes(X1,X2,fill=value)) + geom_tile() + scale_fill_continuous(low="blue",high="red") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggplot(m.PP,aes(X1,X2,fill=value)) + geom_tile() + scale_fill_continuous(low="blue",high="red",na.value="white") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#In the future this is where you consider relatedness among species among plants
+
 
 #package sna, gplot produces some nice networks
-
+#Plot 3d visualization of the hummingbird network
+svg("Hummingbird3d.jpeg")
 gplot(H_H)
+dev.off()
+}
+
+############################################
+#Run Network Function for the entire dataset
+NetworkC(dat,"Total")
+############################################
+
+####################################################
+#Temporal Change in Network Structure
+####################################################
+
+#Split input data into desired paths
+dat$Month<-months(chron(as.character(dat$Date)))
+dat.split<-split(dat,dat$Month,drop=TRUE)
+
+#Compute metrics for each month
+for (x in 1:length(dat.split)){
+  NetworkC(dat.split[[x]],names(dat.split)[[x]])
+}
+
+setwd(home)
+save.image("Thesis/Maquipucuna_SantaLucia/Results/Network/NetworkData.Rdata")
