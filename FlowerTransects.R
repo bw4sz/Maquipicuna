1906652a182f75e3c1eb1da6c26a9bda4ff70d07
bw4sz <benweinstein2010@gmail.com>
2014-06-09 13:45:53 -0400

    years column added to flowertransects.R

--- FlowerTransects.R
+++ FlowerTransects.R
@@ -118,8 +118,12 @@ full.fl$Transect_R<-factor(paste(full.fl$Elevation.Begin,full.fl$Elevation.End,s
 #Go through a series of data cleaning steps, at the end remove all rows that are undesired
 #Repeat for species
 
-Families<-levels(factor(full.fl$Family))
+#one really bad mispelling couldn't be fixed
+full.fl[full.fl$Family %in% "Hydranganceae","Family"]<-"Hydrangeacea"
 
+full.fl[full.fl$Family %in% "???","Family"]<-""
+
+Families<-levels(factor(full.fl$Family))
 
 tax<-tnrs(query = Families, source = "iPlant_TNRS")
 
@@ -128,48 +132,28 @@ for (x in 1:nrow(full.fl)){
   print(x)
   y<-full.fl[x,]
   toMatch<-y$Family
-  full.fl[x,"Iplant_Double"]<-unique(tax[tax$submittedname %in% toMatch,"acceptedname"])
-}
-
-iplant_names<-ResolveNames(names=Families)
-CompareNames(Families,iplant_names)
-
-Fam_Result<-data.frame(Families,iplant_names)
-Fam_Errors<-Fam_Result[Fam_Result$iplant_names %in% "","Families"]
+  if(!toMatch %in% tax$submittedname){next} else{
+  full.fl[x,"Iplant_Family"]<-unique(tax[tax$submittedname %in% toMatch,"acceptedname"])
+}}
 
 #Post to output which plant families need to be address
 print(paste(Fam_Errors,"not found in taxonomy database"))
 
-#Repeat for genus
-Genus<-levels(factor(full.fl$Genus))
-iplant_names<-ResolveNames(names=Genus)
-CompareNames(Genus,iplant_names)
+#Repeat for genus species
+Species<-levels(factor(paste(full.fl$Genus,full.fl$Species,sep=" ")))
 
-Genus_Result<-data.frame(Genus,iplant_names)
-Genus_Errors<-Genus_Result[Genus_Result$iplant_names %in% "","Genus"]
-
-#Post to output which plant families need to be address
-print(paste(Genus_Errors,"not found in taxonomy database"))
-
-#Set the Genus column
-for (x in 1:nrow(full.fl)){
-  y<-full.fl[x,]
-  full.fl[x,"Iplant_Genus"]<-levels(droplevels(Genus_Result[Genus_Result$Genus %in% y$Genus,"iplant_names"] ))   
-}
-
-#Repeat for species
-Species<-levels(factor(paste(full.fl$Iplant_Genus,full.fl$Species,sep="_")))
-iplant_names<-ResolveNames(Species)
-print(CompareNames(Species,iplant_names))
-Species_Result<-data.frame(Species,iplant_names)
+#look up online, skip the blank
+tax<-tnrs(query = Species[-1], source = "iPlant_TNRS")
 
 #Set the Species column
 for (x in 1:nrow(full.fl)){
   y<-full.fl[x,]
-  toMatch<-paste(y$Iplant_Genus,y$Species,sep="_")
-  full.fl[x,"Iplant_Double"]<-levels(droplevels(
-    Species_Result[Species_Result$Species %in% toMatch,"iplant_names"] ))   
-}
+  toMatch<-paste(y$Genus,y$Species,sep=" ")
+  if(toMatch %in% tax$submittedname){
+  full.fl[x,"Iplant_Double"]<-unique(tax[tax$submittedname %in% toMatch,"acceptedname"]   )
+} else {
+  next
+}}
 
 #Lots of cleaning left to do, but that's a start. 
 
@@ -253,11 +237,25 @@ for (j in 1:nrow(full.fl)){
   }
 }
 
+#year column
+
+#Create month column
+full.fl$Year<-NA
+for (j in 1:nrow(full.fl)){
+  x<-full.fl[j,]
+  if(x[["Observer"]] %in% c("Karen","Ben")){
+    full.fl[j,"Year"]<-as.character(years(chron(as.character(x$Date))))
+  }
+  if(x[["Observer"]] %in% "Holger"){
+    full.fl[j,"Year"]<-as.character(years(chron(as.character(x[x$Observer %in% "Holger","Date"]),format="d/m/y")))
+  }
+}
+
 #There is the one unique transect where holger did the transect with us. 
 head(full.fl[is.na(full.fl$month),])
 
-#plot total flowers over time?
-fl.totals<-aggregate(full.fl$Total_Flowers,list(full.fl$Transect_R,full.fl$month,full.fl$Date),sum)
+#plot total flowers over time
+fl.totals<-aggregate(full.fl$Total_Flowers,list(full.fl$Transect_R,full.fl$month,full.fl$Date,full.fl$Year),sum)
 colnames(fl.totals)<-c("Elev","Month","Date","TotalFlowers")
 
 #Write data to file
@@ -278,20 +276,5 @@ ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/FlowerMon
 ggplot(fl.totals,aes(x=as.factor(Month),TotalFlowers,col=Elev)) + geom_point(size=3) + theme_bw()  + geom_smooth(aes(group=Elev)) + facet_wrap(~Elev,scales="free_x") + scale_y_continuous(limits=c(0,5500),breaks=seq(0,5000,1000))
 ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/FlowerElevations.jpeg",height=8,width=10)
 
-#Brief look at time series and taxonomy
-tax<-aggregate(full.fl[,c("Iplant_Double","Iplant_Genus","Family")],list(full.fl$month,full.fl$Transect_R,full.fl$Date),function(x){
-  nlevels(factor(x))})
-
-colnames(tax)[1:3]<-c("month","Elev","Date")
-tax.m<-melt(tax,id.var=c("month","Elev","Date"))
-
-#Taxonomy over time
-ggplot(tax.m,aes(month,value,col=variable)) + geom_point(size=2) + geom_smooth(aes(group=variable),se=FALSE) + facet_grid(.~Elev) + theme_bw() + labs(col="Tax. Level")
-ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/TimeSeriesTaxonomy.jpeg",height=5,width=10)
-
-#Genus to Species Ratios
-ggplot(tax,aes(x=month,col=Elev,y=Iplant_Double/Iplant_Genus)) + geom_point() + geom_smooth(aes(group=1)) + xlab("Month") + facet_grid(~Elev,margins=TRUE) + theme_bw()
-ggsave(filename="Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/TimeSeriesGSratio.jpeg",height=7,width=12)
-
-#Write cleaned flower transect data?
+#Write cleaned flower transect data
 save.image("Thesis/Maquipucuna_SantaLucia/Results/FlowerTransect.Rdata")
