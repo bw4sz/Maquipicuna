42666a381b706c9c1a4b01f2c7fe604ff15bf47d
bw4sz <benweinstein2010@gmail.com>
2014-10-27 10:46:56 -0400

    Added GPS data for flowers

---
+++ GPSFlowerTransects.R
@@ -0,0 +1,106 @@
+#####################################
+#GPS MATCHING AND FLOWER TAXONOMY
+#####################################
+
+#Load GPS
+#Read and convert gpx points to a single dataframe and save it as a shapefile
+require(plotKML)
+require(reshape2)
+require(maptools)
+require(plyr)
+require(stringr)
+require(taxize)
+#Set working directory
+droppath<-"C:/Users/Ben/Dropbox/"
+
+setwd(droppath)
+
+#################################################
+#Read in GPS Data from Holger Field Season
+#################################################
+
+##Repeat for first gps
+g<-list.files("Holger\\Transect_Protocol_Holger\\WayPoints",full.names=TRUE,pattern=".gpx",recursive=TRUE)
+
+#loop through input files and find the errors. 
+gpx<-list()
+for (x in 1:length(g)){
+  print(x)
+  try(
+    gpx[[x]]<-readGPX(g[x],waypoints=TRUE)$waypoints)
+}
+
+#Bind into one dataframe
+gpx.dat<-rbind.fill(rbind.fill(gpx[sapply(gpx,class)=="data.frame"]))
+gpx.dat$name<-as.character(gpx.dat$name)
+
+#Combine gps types
+colnames(gpx.dat)
+
+#create  spatial object
+gps<-SpatialPointsDataFrame(coords=cbind(gpx.dat$lon,gpx.dat$lat),gpx.dat)
+
+#Create month ID column in the GPS data
+gps$MonthID<-sapply(gps$time,function(x){
+  b<-strsplit(as.character(x),"T")[[1]][1]
+  if(is.na(b)){
+    return("S")
+  }
+  return(as.numeric(format(as.POSIXlt(b),"%m")))
+})
+
+
+#Date column
+#Create month ID column in the GPS data
+gps$Date_F<-sapply(gps$time,function(x){
+  b<-strsplit(as.character(x),"T")[[1]][1]
+  if(is.na(b)){
+    return("S")
+  }
+  return(format(as.POSIXlt(b),"%Y-%m-%d"))
+})
+
+
+#Create Year ID column in the GPS data
+gps$YearID<-sapply(gps$Date_F,function(b){
+  yr<-format(as.POSIXlt(b),"%Y")
+  return(as.numeric(yr))
+})
+
+#Round elevation to the nearest 10m 
+gps$ele<-round(as.numeric(as.character(gps$ele)),-1)
+
+#remove weird place holger from values below 100
+gps$GPS.ID<-sapply(gps$name,function(x){
+  a<-as.numeric(as.character(x))
+  if(is.na(a)){
+    return(x)
+  }
+  if(is.numeric(a)){
+    return(a)
+  }
+})
+
+#remove duplicates
+gps_noduplicate<-gps[-duplicated(gps@data),]
+
+#create shapefile
+writePointsShape(gps,"Thesis\\Maquipucuna_SantaLucia\\Data2013\\Shapefiles\\GPSshapeFlowerTransects.shp")
+
+############################################
+##############Merge GPS Info with Data######
+############################################
+
+#There are two video files, that need to be merge
+dat<-read.csv("Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/CleanedHolgerTransect.csv")
+
+head(dat)
+
+#merge
+datg<-merge(dat,gps_noduplicate,by.x=c("GPS_ID","month","year"),by.y=c("GPS.ID","MonthID","YearID"))
+
+dim(dat)
+dim(datg)
+
+#Write camera data to file
+write.csv(datg,"Thesis/Maquipucuna_SantaLucia/Results/FlowerTransects/FlowerTransectClean.csv")
